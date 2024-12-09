4e49a4ef496da526722ad94d71c499b2d10ad960
Allison <allison.schroder.mail@gmail.com>
2024-12-08 16:46:30 -0800

    add link to book on amazon

--- recommendations.js
+++ recommendations.js
@@ -3,11 +3,13 @@ function getRecommendationText(recommendation, subject) {
   if (!recommendation) {
     displayText = "No recommendation found, try again with a different topic.";
   } else {
+    amazonUrl = getAmazonUrlForBook(recommendation);
     displayText = `
                     <h2>Recommended book for subject: ${subject}</h2>
                     <h2>${recommendation.title} by ${recommendation.author}</h2>
                     <h3>Description:</h3>
                     <p>${recommendation.description}</p>
+                    <h3>Amazon link <a href=${amazonUrl} target="_blank">here</a></h3>
                   `;
   }
   return displayText;
@@ -52,7 +54,6 @@ function recommendBooks(
     location,
     subject,
   );
-  console.log(fullSearchString);
   const results = idx.search(fullSearchString);
   console.log(results);
   if (results.length === 0) {
@@ -67,9 +68,9 @@ function recommendBooks(
 }
 
 function cleanInputText(wikipediaText) {
-  console.log(wikipediaText);
   //TODO figure out how to handle topics that have formulas
   let cleanedText = wikipediaText.replace(/:/g, ""); // colon is a keyword for lunr, so remove from incoming text
+  cleanedText = cleanedText.split(/\s+/).slice(0, 1000).join(" ");
   return cleanedText;
 }
 
@@ -77,17 +78,32 @@ function assembleSearchText(cleanedText, searchTerm, genre, location, subject) {
   let prefix =
     searchTerm
       .split(" ")
-      .map((word) => `${word}^100`)
+      .map((word) => `${word}^10`)
       .join(" ") + " ";
   cleanedText = cleanedText + " " + prefix + "\n";
   if (genre) {
-    cleanedText = cleanedText + `genre: ${genre} \n`;
+    cleanedText = cleanedText + `genre: ${genre}^20 \n`;
   }
   if (location) {
-    cleanedText = cleanedText + `setting: ${location} \n`;
+    cleanedText = cleanedText + `setting: ${location}^20 \n`;
   }
   if (subject) {
-    cleanedText = cleanedText + `subject: ${subject} \n`;
+    cleanedText = cleanedText + `subject: ${subject}^20 \n`;
   }
   return cleanedText;
+}
+
+function getAmazonUrlForBook(recommendation) {
+  const baseAmazonUrl = "https://www.amazon.com/s?k=";
+  const titleWords = recommendation.title.split(" ");
+  const authorWords = recommendation.author.split(" ");
+  let bookParams = "";
+  for (const word of titleWords) {
+    bookParams = bookParams + `${word}+`;
+  }
+  for (const word of authorWords) {
+    bookParams = bookParams + `${word}+`;
+  }
+  bookParams = bookParams + "book";
+  return baseAmazonUrl + bookParams;
 }
